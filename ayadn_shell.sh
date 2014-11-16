68b2dbf4b22397e77fdd6bae8efc6a7c6a751ead
bazbt3 <bazbt3@users.noreply.github.com>
2014-11-16 19:32:03 +0000

    Updated to 0.4.
    As system variables - using `export` failed, the only way I can think of to create a variable is to create an external text file. Kludgy. Works.

--- ayadn_shell.sh
+++ ayadn_shell.sh
@@ -7,9 +7,15 @@
 # 2. An understanding that I'm new to Linux - I'm pretty sure this code won't break anything but be careful if you wish to modify it.
 # 3. An understanding that, because I'm new to Linux I've created about 13 documents thus far. All in ~/Documents - easy.
 
+# IMPORTANT - this file lets the user refresh Tmux but fails to exit correctly.
+# See the commented lines below.
+
+# Create ayadn_refresh.txt with default 'Refresh' contents.
+printf 'Refresh' > ~/Documents/ayadn_refresh.txt
+
 clear
-choice=
-until [ "$choice" = "0" ];
+CHOICE=
+until [ "$CHOICE" = "x" ];
   do
     echo ""
     echo "AYADN MENU"
@@ -17,52 +23,66 @@ until [ "$choice" = "0" ];
     echo "1 - ayadn write"
     echo "2 - ayadn reply"
     echo "3 - ayadn pm [channel/alias]"
-    echo "4 - ayadn convo [post number]"
+    echo "4 - ayadn send [channel/alias]"
     echo "5 - ayadn repost [post number]"
     echo ""
-    echo "9 - next window"
+    echo "6 - ayadn convo [post number]"
+    echo "7 - ayadn channels"
     echo ""
-    echo "0 - Exit session"
+    echo "r - Refresh all"
+    echo "n - Next window"
+    echo "x - Exit session"
     echo ""
     echo -n "Enter Selection:"
-    read choice
+    read CHOICE
     echo ""
-    case $choice
+    case $CHOICE
       in
         1 ) clear
             echo "Compose a multiline post:"
-            echo "-------------------------"
+            echo ""
             ayadn write;;
         2 ) clear
             echo "Compose a reply:"
-            echo "-------------"
+            echo ""
             echo "Reply [post number]?"
             read post
             ayadn reply $post;;
         3 ) clear
             echo "Compose a PM:"
-            echo "-------------"
+            echo ""
             echo "PM [channel number/alias?]"
             read channel
             ayadn pm $channel;;
         4 ) clear
+            echo "Send a message to a channel:"
+            echo ""
+            echo "Send [channel/alias]"
+            read channel
+            ayadn send $channel;;
+        5 ) clear
+            echo "Repost:"
+            echo ""
+            echo "Post [post]?"
+            read post
+            ayadn repost $post;;
+        6 ) clear
             echo "Display a conversation:"
-            echo "-----------------------"
+            echo ""
             echo "Conversation [post number]?"
             read post
             ayadn convo $post
             read -p "Press [Enter] to return to menu.";;
-        5 ) clear
-            echo "Repost:"
-            echo "-----------------------"
-            echo "Post [post]?"
-            read post
-            ayadn repost $post;;
-        9 ) clear
+        7 ) clear
+            echo "Display channels"
+            echo ""
+            ayadn channels;;
+        n ) clear
             tmux next-window;;
-        0 ) tmux kill-session
-            exit;;
-        * ) echo "Please enter 1,2,3,4,9 or 0."
+        r ) printf 'Refresh' > ~/Documents/ayadn_refresh.txt
+            tmux kill-session;;
+        x ) printf 'Exit' > ~/Documents/ayadn_refresh.txt
+            tmux kill-server;;
+        * ) echo "Please enter 1,2,3,4,5,6,7,n,r or x. Thanks!"
      esac
-     echo ""
   done
